9adb95ab4ddff1ce6fe212963b9144a3bc714ed3
Priambodo-Kurniawan <priambodo.2009@gmail.com>
2022-07-22 13:12:51 +0700

    1 file modified

--- src/components/Action.js
+++ src/components/Action.js
@@ -18,7 +18,7 @@ function Action({ data }) {
       <a
         target="_blank"
         href={linkWa()}
-        class="btn gap-2 bg-green-600 border-0 mr-3"
+        class="btn gap-2 bg-green-600 border-0 mr-3 mb-3"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -34,7 +34,7 @@ function Action({ data }) {
             d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
           />
         </svg>
-        Share to whatsapp
+        Bagikan ke Whatsapp
       </a>
       <button
         onClick={closeModal}
